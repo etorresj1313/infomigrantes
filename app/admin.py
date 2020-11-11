333bb6da059833da80e8c415fd11e41b06d5603e
MarioMoya77 <mari.moyam@alumnos.duoc.cl>
2020-11-10 23:41:03 -0300

    Add Form Contacto

--- app/admin.py
+++ app/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import Users, Agregar
 
+
 class AgregarAdmin(admin.ModelAdmin):
     list_display = ["rut", "nombres", "apellidos", "edad", "fecha_nacimiento", "nacionalidad", "direccion", "correo", "telefono"]
 
 admin.site.register(Users)
 admin.site.register(Agregar, AgregarAdmin)
+
 
 
