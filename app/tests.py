bafa305bb80ad619e2661dcc0904023cca74ae37
Eduardo Torres <e.torresj@alumnos.duoc.cl>
2020-12-08 09:23:38 -0300

    add test view

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+class ViewsTestCase(TestCase):
+    def test_index_loads_properly(self):
+        """The index page loads properly"""
+        response = self.client.get('http://127.0.0.1:8000')
+        self.assertEqual(response.status_code, 200)
